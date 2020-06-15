ac00405d003a9fa3e3983594dc09d631f7812181
Arnold Therigan <arnoldtherigan15@gmail.com>
2020-06-15 13:40:20 +0700

    deploy to heroku

--- server/app.js
+++ server/app.js
@@ -1,14 +1,18 @@
-require('dotenv').config()
 const express = require('express')
-const errorHandler = require('./middlewares/errorHandler')
 const app = express()
+
+if(process.env.NODE_ENV == 'development') {
+    require('dotenv').config()
+    const morgan = require('morgan')
+    app.use(morgan('dev'))
+}
+
+const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
-const morgan = require('morgan')
 const router = require('./routes')
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
-app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(router)
