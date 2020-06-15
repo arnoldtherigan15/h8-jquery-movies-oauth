121a80bbd1e0ab4ae2f834aaeb1833f9abd2b014
Arnold Therigan <arnoldtherigan15@gmail.com>
2020-06-15 13:45:42 +0700

    deploy to heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const userRouter = require('./userRouter')
 const movieRouter = require('./movieRouter')
 
-router.get('/', (req,res) => req.json({msg: 'welcome to netflix'}))
+router.get('/', (req,res) => res.json({msg: 'welcome to netflix'}))
 
 router.use('/users', userRouter)
 router.use('/movies', movieRouter)
